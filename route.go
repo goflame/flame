fa30ec0651fcc38962dfe08d8527b2f8be387422
Martin Binder <72764328+bndrmrtn@users.noreply.github.com>
2024-02-14 11:35:25 +0100

    Update route.go

--- route.go
+++ route.go
@@ -1,11 +1,31 @@
 package flame
 
-func findRoute(name string, _ Map, f *Flame) string {
-	// TODO: Add prop support
+import (
+	"strings"
+	"fmt"
+)
+
+func findRoute(name string, m Map, f *Flame) string {
+	// FIXIT: 2 Slashes at start
 	routes := f.Router.Export()
 	for _, route := range routes {
 		if route.ExportName() == name {
-			return route.Path
+			var built string
+			sp := strings.Split(route.Path, "/")
+			for _, part := range sp {
+				if !strings.HasPrefix(part, "{") {
+					built += "/" + part
+				} else {
+					part = part[1 : len(part)-1]
+					if strings.Contains(part, "<") {
+						sp = strings.Split(part, "<")
+						built += fmt.Sprintf("/%s", m[sp[0]])
+					} else {
+						built += fmt.Sprintf("/%s", m[part])
+					}
+				}
+			}
+			return built
 		}
 	}
 	return ""
